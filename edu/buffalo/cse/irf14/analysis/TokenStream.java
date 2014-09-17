90ffdbaabbec970294c24371fe7209645de0639a
animesh-kumar <animesh143@gmail.com>
2014-09-16 22:22:34 -0400

    Updating remove method in TokenStream such that it's NOOP in the beginning and end of the stream

--- edu/buffalo/cse/irf14/analysis/TokenStream.java
+++ edu/buffalo/cse/irf14/analysis/TokenStream.java
@@ -3,151 +3,154 @@
  */
 package edu.buffalo.cse.irf14.analysis;
 
-
-
 import java.util.Iterator;
 import java.util.List;
 
 /**
- * @author nikhillo
- * Class that represents a stream of Tokens. All {@link Analyzer} and
- * {@link TokenFilter} instances operate on this to implement their
- * behavior
+ * @author nikhillo Class that represents a stream of Tokens. All
+ *         {@link Analyzer} and {@link TokenFilter} instances operate on this to
+ *         implement their behavior
  */
-public class TokenStream implements Iterator<Token>{
+public class TokenStream implements Iterator<Token> {
 
 	/**
-	 *  variable to hold the list of tokens
+	 * variable to hold the list of tokens
 	 */
 	private List<Token> tokenList;
-	
+
 	/**
-	 * variable that maintains the index to individual tokens in the  tokenList
+	 * variable that maintains the index to individual tokens in the tokenList
 	 */
-	public int index=0;
-	
+	public int index = 0;
+
 	/**
-	 * variable which will contain a copy of the main index . It can be used
-	 * to get the next token without modifying the main index.
+	 * variable which will contain a copy of the main index . It can be used to
+	 * get the next token without modifying the main index.
 	 */
-	public int cloneIndex=0;
-	
+	public int cloneIndex = 0;
+
 	/**
 	 * Constructor that initializes the token stream with the list
 	 */
-	public TokenStream(List<Token> token){
+	public TokenStream(List<Token> token) {
 		tokenList = token;
 		index = 0;
 	}
+
 	/**
-	 * Method that checks if there is any Token left in the stream
-	 * with regards to the current pointer.
-	 * DOES NOT ADVANCE THE POINTER
+	 * Method that checks if there is any Token left in the stream with regards
+	 * to the current pointer. DOES NOT ADVANCE THE POINTER
+	 * 
 	 * @return true if at least one Token exists, false otherwise
 	 */
 	@Override
 	public boolean hasNext() {
 		// TODO YOU MUST IMPLEMENT THIS
-		/**As soon as size becomes equal to index that means the last item in the list has
-	     * already been iterated
+		/**
+		 * As soon as size becomes equal to index that means the last item in
+		 * the list has already been iterated
 		 */
-		if(tokenList.size() != index)
+		if (tokenList.size() != index)
 			return true;
 		else
 			return false;
 	}
 
 	/**
-	 * Method to return the next Token in the stream. If a previous
-	 * hasNext() call returned true, this method must return a non-null
-	 * Token.
-	 * If for any reason, it is called at the end of the stream, when all
-	 * tokens have already been iterated, return null
+	 * Method to return the next Token in the stream. If a previous hasNext()
+	 * call returned true, this method must return a non-null Token. If for any
+	 * reason, it is called at the end of the stream, when all tokens have
+	 * already been iterated, return null
 	 */
 	@Override
 	public Token next() {
 		// TODO YOU MUST IMPLEMENT THIS
-		if(hasNext()){
+		if (hasNext()) {
 			cloneIndex = index;
 			return tokenList.get(index++);
-	    }
-		else{
+		} else {
 			return null;
 		}
 	}
-	
+
 	/**
-	 * Method to remove the current Token from the stream.
-	 * Note that "current" token refers to the Token just returned
-	 * by the next method. 
-	 * Must thus be NO-OP when at the beginning of the stream or at the end
+	 * Method to remove the current Token from the stream. Note that "current"
+	 * token refers to the Token just returned by the next method. Must thus be
+	 * NO-OP when at the beginning of the stream or at the end
 	 */
 	@Override
 	public void remove() {
 		// TODO YOU MUST IMPLEMENT THIS
-		tokenList.remove(--index);
+		if (index < tokenList.size() && index > 0) {
+			tokenList.remove(--index);
+		}
 	}
-	
+
 	/**
-	 * Method to reset the stream to bring the iterator back to the beginning
-	 * of the stream. Unless the stream has no tokens, hasNext() after calling
+	 * Method to reset the stream to bring the iterator back to the beginning of
+	 * the stream. Unless the stream has no tokens, hasNext() after calling
 	 * reset() must always return true.
 	 */
 	public void reset() {
-		//TODO : YOU MUST IMPLEMENT THIS
+		// TODO : YOU MUST IMPLEMENT THIS
 		index = 0;
 	}
-	
+
 	/**
 	 * Method to append the given TokenStream to the end of the current stream
-	 * The append must always occur at the end irrespective of where the iterator
-	 * currently stands. After appending, the iterator position must be unchanged
-	 * Of course this means if the iterator was at the end of the stream and a 
-	 * new stream was appended, the iterator hasn't moved but that is no longer
-	 * the end of the stream.
-	 * @param stream : The stream to be appended
+	 * The append must always occur at the end irrespective of where the
+	 * iterator currently stands. After appending, the iterator position must be
+	 * unchanged Of course this means if the iterator was at the end of the
+	 * stream and a new stream was appended, the iterator hasn't moved but that
+	 * is no longer the end of the stream.
+	 * 
+	 * @param stream
+	 *            : The stream to be appended
 	 */
 	public void append(TokenStream stream) {
-		//TODO : YOU MUST IMPLEMENT THIS
+		// TODO : YOU MUST IMPLEMENT THIS
 	}
-	
+
 	/**
-	 * Method to get the current Token from the stream without iteration.
-	 * The only difference between this method and {@link TokenStream#next()} is that
-	 * the latter moves the stream forward, this one does not.
-	 * Calling this method multiple times would not alter the return value of {@link TokenStream#hasNext()}
+	 * Method to get the current Token from the stream without iteration. The
+	 * only difference between this method and {@link TokenStream#next()} is
+	 * that the latter moves the stream forward, this one does not. Calling this
+	 * method multiple times would not alter the return value of
+	 * {@link TokenStream#hasNext()}
+	 * 
 	 * @return The current {@link Token} if one exists, null if end of stream
-	 * has been reached or the current Token was removed
+	 *         has been reached or the current Token was removed
 	 */
 	public Token getCurrent() {
-		//TODO: YOU MUST IMPLEMENT THIS
+		// TODO: YOU MUST IMPLEMENT THIS
 		return null;
 	}
-	
+
 	/**
-	 * Method to get the next Token from the stream without iteration.
-	 * This method {@link TokenStream#getNext()}  does not move the stream forward.
+	 * Method to get the next Token from the stream without iteration. This
+	 * method {@link TokenStream#getNext()} does not move the stream forward.
 	 */
 	public Token getNext() {
 		cloneIndex = index;
-		if(hasNext()){
+		if (hasNext()) {
 			return tokenList.get(cloneIndex);
-	    }
-		else{
+		} else {
 			return null;
 		}
 	}
-	
+
 	/**
 	 * getter for index
+	 * 
 	 * @return
 	 */
-	public int getIndex(){
+	public int getIndex() {
 		return index;
 	}
-	
+
 	/**
 	 * setter for index
+	 * 
 	 * @param index
 	 */
 	public void setIndex(int index) {
