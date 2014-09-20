36fb20c483428f1425a6aef52956e4d575e7ffc4
animesh-kumar <animesh143@gmail.com>
2014-09-20 05:47:18 -0400

    TokenFilterType - Cleanup

--- edu/buffalo/cse/irf14/analysis/TokenFilterType.java
+++ edu/buffalo/cse/irf14/analysis/TokenFilterType.java
@@ -4,19 +4,9 @@
 package edu.buffalo.cse.irf14.analysis;
 
 /**
- * @author nikhillo
- * Enum that defines different {@link TokenFilter} types
+ * @author nikhillo Enum that defines different {@link TokenFilter} types
  */
 public enum TokenFilterType {
 	SYMBOL, DATE, NUMERIC, CAPITALIZATION, STOPWORD, STEMMER, ACCENT, SPECIALCHARS;
-	
-	public Class getClass(TokenFilterType type){
-		TokenFilter filter = null;
-		switch(type){
-		   case NUMERIC:
-			   filter=new NumberRule(null);
-			   break;
-		}
-		return filter.getClass();
-	}
+
 };
