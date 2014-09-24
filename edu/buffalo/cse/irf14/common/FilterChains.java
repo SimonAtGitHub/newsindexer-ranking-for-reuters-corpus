89a9e1225048117878d858901a2ee82b6048bcb8
animesh-kumar <animesh143@gmail.com>
2014-09-23 22:25:22 -0400

    Updated FilterChain for Date

--- edu/buffalo/cse/irf14/common/FilterChains.java
+++ edu/buffalo/cse/irf14/common/FilterChains.java
@@ -7,6 +7,11 @@ import edu.buffalo.cse.irf14.analysis.TokenFilterType;
 
 public class FilterChains {
 
-	public static final TokenFilterType [] FILTERS_FOR_CONTENT={TokenFilterType.NUMERIC,TokenFilterType.STOPWORD};
-}
+	public static final TokenFilterType[] FILTERS_FOR_CONTENT = {
+			TokenFilterType.CAPITALIZATION, TokenFilterType.SYMBOL,
+			TokenFilterType.SPECIALCHARS, TokenFilterType.DATE,
+			TokenFilterType.NUMERIC, TokenFilterType.STOPWORD,
+			TokenFilterType.STEMMER };
 
+	public static final TokenFilterType[] FILTERS_FOR_NEWSDATE = { TokenFilterType.DATE };
+}
